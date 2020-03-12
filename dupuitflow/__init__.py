c5b55ca2831cb4a2ff7e1834f43bd7c242abbc12
houben <timo.houben@ufz.de>
2020-03-12 11:08:25 +0100

    bufgix

--- dupuitflow/__init__.py
+++ dupuitflow/__init__.py
@@ -10,14 +10,12 @@ Subpackages
 
 .. autosummary::
     core
-    test_dupuitflow
 
 """
 from __future__ import absolute_import
 
 from dupuitflow._version import __version__
 from dupuitflow import core
-from dupuitflow import test_dupuitflow
 
 __all__ = ["__version__"]
 __all__ += ["core"]
